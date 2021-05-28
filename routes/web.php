aecc7d63562dfca19b7b26df85b6b910596c2934
23tai <zorn23sky@gmail.com>
2021-05-28 09:41:29 +0900

    ファイル全体の整理前のコミット

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Talk_peopleController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
