a3df0f4c7f71cef1629bc373ecd06b266758dc3d
23tai <zorn23sky@gmail.com>
2021-05-28 13:43:38 +0900

    ActivityController.phpのリファクタリング（コードの要らない部分を消した）

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -15,33 +15,15 @@ class ActivityController extends Controller
      */
     public function index()
     {
-        //
-        $myId = Auth::id();
-        // $myAccount = User::find($myId);
         $myAccount = Auth::user();
 
-        // dd(User::where('id', 1)->first()->alls()->where('team_id', 3)->get());
-
-
-
         // 一覧表示用にフォローワーを自分をフォローしてくれた順で表示
-        // $accounts_follower = User::find($myId)->show_follower_activity()->get();
         $accounts_follower = $myAccount->getFollowerActivity();
         
-
-        // ここのidentify_idはdetails.blade.phpに移動した時のbackとかに必要　⇦　これはもう要らないコントローラを細かく分けたから
-        // 　　でもまだ identify_idは必要である、follow_lists.invokeで必要だから。
-        // $identify_id = 'activity';
-            // ↑ フォローボタンを非同期にしたから必要なくなった
-
         return view('myService.activity')->with([
             'accounts_follower' => $accounts_follower,
             // ↓ それぞれのアカウントが自分がフォローしているかどうかを調べるfollow_checkで使う
             'myAccount' => $myAccount,
-            // 'identify_id' => $identify_id,
-            // ↓ フォローされた時間を表示する時にfollowテーブルの自分と相手のレコードを指定する時に使う
-            // created_atを取得
-            // 'myId' => $myId,
         ]);
     }
 
@@ -55,23 +37,16 @@ class ActivityController extends Controller
      */
     public function show(User $user, Request $request)
     {
-        //
         $identify_id = 'activity';
         $user_id = $user->id;
 
-        $myId = Auth::id();
-        // $myAccount = User::find($myId);
         $myAccount = Auth::user();
 
-
-
         // どの人の詳細を表示させるかをuser_idで受け取ってその人をフォローしているかを
-        // $follow_check = $myAccount->show_follow()->where('receive_user_id', $user_id)->first();
         $follow_check = $myAccount->followCheck($user_id);
         
         // どの人の詳細を表示させるかをuser_idで受け取ってその人のアカウントを取得
         $hisAccount = User::find($user_id);
-
 
         return view('myService.details')->with([
             'identify_id' => $identify_id,
