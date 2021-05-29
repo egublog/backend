b0df5b8596f8a21ea1a4e1bff8826d219c69b9be
23tai <zorn23sky@gmail.com>
2021-05-29 10:38:44 +0900

    FollowController.phpのリファクタリング（コードの要らない部分を消した）

--- app/Http/Controllers/FollowController.php
+++ app/Http/Controllers/FollowController.php
@@ -8,25 +8,6 @@ use Illuminate\Support\Facades\Auth;
 
 class FollowController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -36,63 +17,12 @@ class FollowController extends Controller
      */
     public function store(Request $request)
     {
-        //
         $myAccount = Auth::user();
 
-        // フォローする
-        // $myAccount->show_follow()->attach($request->user_id);
+        // フォローする followsテーブルに自分のidと相手のidを追加する
         $myAccount->followAttach($request->user_id);
-        
-        // $myAccount = Auth::user();
-        // if ($myAccount->image == null) {
-        //     $myAccount->image = 0;
-        //     $myAccount->save();
-        // }
-
-        
-
-
-        // 自分のプロフィール表示用に自分のアカウント情報を付ける (myAccount)
-        // 自分の画像表示用にmyIdを付ける (myId)
-        return view('myService.home')->with([
-            'myAccount' => $myAccount,
-            // 'myId' => $myId,
-        ]);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
 
     /**
      * Remove the specified resource from storage.
@@ -102,11 +32,9 @@ class FollowController extends Controller
      */
     public function destroy($user_id)
     {
-        //
         $myAccount = Auth::user();
 
-        // フォローしていたら外す、してなかったらフォローする
-        // $myAccount->show_follow()->detach($user_id);
+        // フォローを外す followsテーブルに自分のidと相手のidを削除する
         $myAccount->followDetach($user_id);
     }
 }
