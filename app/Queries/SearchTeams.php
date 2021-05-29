ad057e24ed4c01d0910425057c6b11bd8958b679
23tai <zorn23sky@gmail.com>
2021-05-29 11:17:30 +0900

    App\Queries\SearchTeams.phpのリファクタリング（コードの要らない部分を消した）

--- app/Queries/SearchTeams.php
+++ app/Queries/SearchTeams.php
@@ -10,7 +10,6 @@ use App\Team;
 final class SearchTeams
 {
   
-
   public static function get($team_string)
   {
     return Team::where('team_name', 'like', '%' . $team_string . '%')->pluck('id')->all();
