0bf6a910a755bb000ad668f61b53098766321acd
23tai <zorn23sky@gmail.com>
2021-05-29 10:38:25 +0900

    Talk_userContentController.phpのリファクタリング（コードの要らない部分を消した）

--- app/Http/Controllers/Talk_userContentController.php
+++ app/Http/Controllers/Talk_userContentController.php
@@ -24,6 +24,7 @@ class Talk_userContentController extends Controller
      */
     public function index(User $user, Request $request)
     {
+        //                  ↑ ここはネストしたrouteだからパラメータを取れる。
 
 
         // エラーでバリデーションのリダイレクト先がここだけど大丈夫か
@@ -31,73 +32,42 @@ class Talk_userContentController extends Controller
         // て事はtalkSendクリックイベントで必要なのはここにある最低限のデータとユーザーが打ったトークデータだけか
 
 
-
-
-        //                  ↑　ここはネストしたrouteだからパラメータを取れる。
         $myId = Auth::id();
         $user_id = $user->id;
 
-        // 自分が関係しているtalk_listsテーブルを新しい順で全て撮ってくる 尚、上の処理のおかげで被っている事は無い
-        // $talk_lists = Talk_list::where('from', $myId)->orWhere('to', $myId)->orderBy('created_at', 'desc')->get();
+        // 自分が関係しているtalk_listsテーブルを新しい順で全て撮ってくる 尚、Talk_listsテーブルへの保存方法のおかげで被っている事は無い
         $talk_lists = Talk_list::fromToEqual($myId)->get();
 
         // その中で自分のidじゃ無いidだけ撮って来て配列$account_idsに入れる
-        // $account_ids = array();
-        // foreach ($talk_lists as $talk_list) {
-        //     if ($talk_list->to != $myId) {
-        //         $account_ids[] = $talk_list->to;
-        //     }
-        //     if ($talk_list->from != $myId) {
-        //         $account_ids[] = $talk_list->from;
-        //     }
-        // }
         $opponent_ids = TalkList::getOpponentIds($talk_lists, $myId);
 
 
 
-        //    そのidをもとにfindでuser取ってきてアカウントのオブジェクトの配列を作る
-        // $talk_lists_accounts = array();
-        // foreach ($account_ids as $id) {
-        //     $talk_lists_accounts[] = User::find($id);
-        // }
+        //  そのidをもとにfindで相手のuserを取ってきてアカウントのオブジェクトの配列を作る 順番大事！
         $talk_lists_accounts = TalkList::getTalkListAccounts($opponent_ids);
-        // $talk_lists_accounts = User::find($opponent_ids);
-        // dd($talk_lists_accounts);
 
 
-        // ここで相手が自分に送信したtalkテーブルのレコードのyetカラムをtrueにする、よって既読になる
-        // $yetColumns = Talk::where('from', $user_id)->where('to', $myId)->where('yet', false)->get();
+        // ここで相手が自分に送ったトークデータでTalksテーブルのyetカラムがfalseのものを取ってくる
         $talkDatas = Talk::yetColumnsFalse($myId, $user_id)->get();
 
-        // if ($talkDatas)
-        //     foreach ($talkDatas as $talkData) {
-        //         $talkData->yet = true;
-        //         $talkData->save();
-        //     }
+        //   ここで$talkDatasがあればそのtalksテーブルのyetカラムをtrueにする
         if ($talkDatas) {
             TalkList::changeYetColumnsTrue($talkDatas);
         }
 
 
-        // ここで自分と相手に関わるtalkテーブルのカラムを全て取得する。
-        // $talkDatas = Talk::where('from', $myId)->where('to', $user_id)->orWhere('from', $user_id)->where('to', $myId)->orderBy('created_at', 'asc')->get();
-
-
         $hisAccount = User::find($user_id);
 
-
-
-        // $talkDatasDesc = Talk::where('from', $myId)->where('to', $user_id)->orWhere('from', $user_id)->where('to', $myId)->orderBy('created_at', 'desc')->limit(20)->with('user')->get();
+        // ここで自分と相手のトークデータの中で最新のレコードを20個取ってくる
         $talkDatasDesc = Talk::TalkDatasLatestLimit($myId, $user_id, 20)->with('user')->get();
 
-        // $talkDatas = $talkDatasDesc->reverse()->values();
+        // トークは古いのが一番上でそこから新しくなるから最新のトークデータを古い順に並び変える
         $talkDatas = CommonService::reverseCollection($talkDatasDesc);
 
         $identify_id = $request->identify_id;
 
 
-
-        // ここでtalk_〇〇の値の talk_ を取る！  つまりtalk_〇〇があるのはdetails.blade.phpだけ
+        // ここでidentify_idのtalk_〇〇の値の talk_ を取る！  つまりtalk_〇〇があるのはdetails.blade.phpだけ
         if (IdentifyId::talkFind($identify_id)) {
             $identify_id = 'find';
         } elseif (IdentifyId::talkActivity($identify_id)) {
@@ -109,7 +79,7 @@ class Talk_userContentController extends Controller
         }
 
 
-        // findだったら(era_id) (team_string)を付ける
+        // identify_idがfindだったら(era_id) (team_string)を付ける
         if (IdentifyId::find($identify_id)) {
             return view('myService.talk_show')->with([
                 'talkDatas' => $talkDatas,
@@ -143,33 +113,25 @@ class Talk_userContentController extends Controller
 
     public function axios_userChange(User $user)
     {
-        //                  ↑　ここはネストしたrouteだからパラメータを取れる。
+        //                  ↑ ここはネストしたrouteだからパラメータを取れる。
         $myId = Auth::id();
         $user_id = $user->id;
 
 
-        // ここで相手が自分に送信したtalkテーブルのレコードのyetカラムをtrueにする、よって既読になる
-        // $yetColumns = Talk::where('from', $user_id)->where('to', $myId)->get();
+       
+
+        // ここで相手が自分に送ったトークデータでTalksテーブルのyetカラムがfalseのものを取ってくる
         $talkDatas = Talk::yetColumnsFalse($myId, $user_id)->get();
 
-
-
-        // if (isset($yetColumns->first()->from))
-        //     foreach ($yetColumns as $yetColumn) {
-        //         $yetColumn->yet = true;
-        //         $yetColumn->save();
-        //     }
+        //   ここで$talkDatasがあればそのtalksテーブルのyetカラムをtrueにする
         if ($talkDatas) {
             TalkList::changeYetColumnsTrue($talkDatas);
         }
 
-
-
-        // $talkDatasDesc = Talk::where('from', $myId)->where('to', $user_id)->orWhere('from', $user_id)->where('to', $myId)->orderBy('created_at', 'desc')->limit(20)->with('user')->get();
+        // ここで自分と相手のトークデータの中で最新のレコードを20個取ってくる
         $talkDatasDesc = Talk::TalkDatasLatestLimit($myId, $user_id, 20)->with('user')->get();
 
-
-        // $talkDatas = $talkDatasDesc->reverse()->values();
+        // トークは古いのが一番上でそこから新しくなるから最新のトークデータを古い順に並び帰る
         $talkDatas = CommonService::reverseCollection($talkDatasDesc);
 
 
@@ -188,23 +150,18 @@ class Talk_userContentController extends Controller
 
     public function axios_talkUpdate(User $user, Request $request)
     {
-
-
-        //                  ↑　ここはネストしたrouteだからパラメータを取れる。
+        //                  ↑ ここはネストしたrouteだからパラメータを取れる。
         $myId = Auth::id();
         $user_id = $user->id;
 
-
-
+        // 無限スクロールの為の番号(pageNumber)が送られてくるからそれに20を掛ける
         $limitNumber = $request->pageNumber * 20;
 
-        // $talkDatasDesc = Talk::where('from', $myId)->where('to', $user_id)->orWhere('from', $user_id)->where('to', $myId)->orderBy('created_at', 'desc')->limit($limitNumber)->with('user')->get();
+        // ここで自分と相手のトークデータの中で最新のレコードを$limitNumberの数だけ取ってくる
         $talkDatasDesc = Talk::TalkDatasLatestLimit($myId, $user_id, $limitNumber)->with('user')->get();
 
-
-        // $talkDatas = $talkDatasDesc->reverse()->values();
+        // トークは古いのが一番上でそこから新しくなるから最新のトークデータを古い順に並び帰る
         $talkDatas = CommonService::reverseCollection($talkDatasDesc);
-
 
 
         $talkArray = [
@@ -250,7 +207,6 @@ class Talk_userContentController extends Controller
             $myId = Auth::id();
 
             // トークを送信したタイミングで自分と相手のtalk_listテーブルのレコードを取ってくる
-            // $existing_talk_list =  Talk_list::where('from', $myId)->where('to', $user_id)->orWhere('from', $user_id)->where('to', $myId)->first();
             $existing_talk_list =  Talk_list::ourTalkList($myId, $user_id)->first();
 
             // それがあればtalk_listsテーブルに$myId or $user_id が会ったら消す
@@ -258,97 +214,52 @@ class Talk_userContentController extends Controller
                 $existing_talk_list->delete();
             }
 
-            // で新しくtalk_listsテーブルに追加する。
-            // $new_talk_list = new Talk_list();
-            // $new_talk_list->from = $myId;
-            // $new_talk_list->to = $user_id;
-            // $new_talk_list->save();
-
+            // で新しくtalk_listsテーブルに自分と相手のidを追加する 。
             $new_talk_list = new Talk_list();
             $new_talk_list->saveNewTalkList($myId, $user_id);
 
-
-            // 自分が関係しているtalk_listsテーブルを新しい順で全て撮ってくる 尚、上の処理のおかげで被っている事は無い
-            // $talk_lists = Talk_list::where('from', $myId)->orWhere('to', $myId)->orderBy('created_at', 'desc')->get();
+            // 自分が関係しているtalk_listsテーブルを新しい順で全て撮ってくる 尚、Talk_listsテーブルへの保存方法のおかげで被っている事は無い
             $talk_lists = Talk_list::fromToEqual($myId)->get();
 
 
             // その中で自分のidじゃ無いidだけ撮って来て配列$account_idsに入れる
-            // $account_ids = array();
-            // foreach ($talk_lists as $talk_list) {
-            //     if ($talk_list->to != $myId) {
-            //         $account_ids[] = $talk_list->to;
-            //     }
-            //     if ($talk_list->from != $myId) {
-            //         $account_ids[] = $talk_list->from;
-            //     }
-            // }
             $opponent_ids = TalkList::getOpponentIds($talk_lists, $myId);
-            // ↑　参画　これはカスタムCollectionを使えばわざわざファサードからやらなくても$talk_list->で実行できる
+            // ↑ 参画 これはカスタムCollectionを使えばわざわざファサードからやらなくても$talk_list->で実行できる
 
-            //    そのidをもとにfindでuser取ってきてアカウントのオブジェクトの配列を作る
-            // $talk_lists_accounts = array();
-            // foreach ($account_ids as $id) {
-            //     $talk_lists_accounts[] = User::find($id);
-            // }
+            //  そのidをもとにfindでuser取ってきてアカウントのオブジェクトの配列を作る  順番大事！
             $talk_lists_accounts = TalkList::getTalkListAccounts($opponent_ids);
 
 
 
-            // 送信したトークデータを空のデータじゃ無かったら保存する、、 yetカラムは0を入れる 多分falseでもいい
-
+            // 送信したトークデータを空のデータじゃ無かったら保存する、、 yetカラムは0を入れる
             if ($request->message != "") {
                 $talkData = new Talk();
-                // $talkData->talk_data = $request->message;
-                // $talkData->from = $myId;
-                // $talkData->to = $user_id;
-                // $talkData->yet = false;
-                // $talkData->talkCheck = false;
-                // $talkData->save();
                 $talkData->saveNewTalk($request->message, $myId, $user_id);
             }
 
+            // ↓ ここからの処理は非同期でもその日,初めてのトークだったらその日の日付を表示すると言う機能の為の下処理
 
-            // $talkDataOneBefore = Talk::where('from', $myId)->where('to', $user_id)->orWhere('from', $user_id)->where('to', $myId)->orderBy('created_at', 'desc')->offset(1)->limit(1)->first();
+            // ここでは自分と相手のトークデータの最新の一個前のレコードを取ってくる
             $talkDataOneBefore = Talk::TalkDataOneBefore($myId, $user_id)->first();
 
             // first()で取ってくると何もなかった時にnullが入ってくる。
             // get()で取ってくると何もなかった時に collectionの{#items: []}が返ってくるからその違いに注意
 
-            // $talkDataNow = Talk::where('from', $myId)->where('to', $user_id)->orWhere('from', $user_id)->where('to', $myId)->orderBy('created_at', 'desc')->first();
+            // ここでは自分と相手のトークデータの最新のレコードを取ってくる
             $talkDataNow = Talk::TalkDataNow($myId, $user_id)->first();
 
-            // dd($talkDataNow);
-
-
-
-            // if ($talkDataOneBefore == null) {
-            //     $talkDataNow->talkCheck = true;
-            //     $talkDataNow->save();
-            // } else {
-            //     if ($talkDataOneBefore->created_at->format('n/j') != $talkDataNow->created_at->format('n/j')) {
-            //         $talkDataNow->talkCheck = true;
-            //         $talkDataNow->save();
-            //     }
-            // }
+            // $talkDataOneBeforeと$talkDataNowの日付を比較して同じだったらtalkCheckカラム(boolean型)にfalse違ったらtrueをいれる($talkDataOneBeforeが存在しなかった場合はtrueをいれる）
             $talkDataNow->saveTalkCheckColumn($talkDataOneBefore);
 
 
-            // $talkDatasDesc = Talk::where('from', $myId)->where('to', $user_id)->orWhere('from', $user_id)->where('to', $myId)->orderBy('created_at', 'desc')->limit(20)->with('user')->get();
+            // ここで自分と相手のトークデータの中で最新のレコードを20個取ってくる
             $talkDatasDesc = Talk::TalkDatasLatestLimit($myId, $user_id, 20)->with('user')->get();
 
-
-            // $talkDatas = $talkDatasDesc->reverse()->values();
+            // トークは古いのが一番上でそこから新しくなるから最新のトークデータを古い順に並び変える
             $talkDatas = CommonService::reverseCollection($talkDatasDesc);
 
-
-
-            $hisAccount = User::find($user_id);
-
-
-
-
-            // findだったらera_id, team_stringを付ける
+            
+            // identify_idがfindだったらera_id, team_stringを付ける
             if (IdentifyId::find($identify_id)) {
                 $talkArray = [
                     'talkDatas' => $talkDatas,
@@ -367,48 +278,5 @@ class Talk_userContentController extends Controller
         }
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+    
 }
