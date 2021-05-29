a3d7813651d3c1f6d0192446baa785ab068a4b4e
23tai <zorn23sky@gmail.com>
2021-05-29 11:46:40 +0900

    開発用のメモフォルダを削除

--- resources/views/nandemo/nanndemo3.blade.php
+++
@@ -1,5 +0,0 @@
-@if ($identify_id == 'find')
-    <talk-show :identify-id="{{ json_encode($identify_id) }}" :initial-user-id="{{ json_encode($user_id) }}" :era-id="{{ json_encode($era_id) }}" :team-string="{{ json_encode($team_string) }}"></talk-show>
-    @else
-    <talk-show :identify-id="{{ json_encode($identify_id) }}" :initial-user-id="{{ json_encode($user_id) }}"></talk-show>
-    @endif
