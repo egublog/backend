9520df134454ffbf974bac3880bf13b192125246
23tai <zorn23sky@gmail.com>
2021-05-29 11:06:17 +0900

    talk_show.blade.phpのリファクタリング（コードの要らない部分を消した）

--- resources/views/myService/talk_show.blade.php
+++ resources/views/myService/talk_show.blade.php
@@ -6,8 +6,6 @@
 
 <main>
 
-
-
     @if ($identify_id == 'find')
     <talk-show :identify-id="{{ json_encode($identify_id) }}" :initial-user-id="{{ json_encode($user_id) }}" :initial-talk-datas="{{ json_encode($talkDatas) }}" :initial-his-account="{{ json_encode($hisAccount) }}" :initial-my-id="{{ json_encode($myId) }}" :initial-talk-lists-accounts="{{ json_encode($talk_lists_accounts) }}" :era-id="{{ json_encode($era_id) }}" :team-string="{{ json_encode($team_string) }}">
     </talk-show>
@@ -16,11 +14,7 @@
     </talk-show>
     @endif
 
-
-
 </main>
-
-
 
 
 
