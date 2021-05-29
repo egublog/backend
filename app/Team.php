4b5589412b4734ece208157e1eb7d8947ec434d4
23tai <zorn23sky@gmail.com>
2021-05-29 10:49:38 +0900

    Team.phpのリファクタリング（コードの要らない部分を消した）

--- app/Team.php
+++ app/Team.php
@@ -6,27 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Team extends Model
 {
-    //
-    public function alls() {
+    // リレーション ↓↓
+    public function alls()
+    {
         return $this->hasMany('App\All');
     }
 
 
 
-        // ↓ scope系
-public function scopeTeamNameEqual($query, $team_string)
-{
-    return $query->where('team_name', $team_string);
-}
+    // ↓↓ scope系
+    public function scopeTeamNameEqual($query, $team_string)
+    {
+        return $query->where('team_name', $team_string);
+    }
 
-            // ↓ データベース保存、削除系
-public function saveTeam($team_string)
-{
-    $this->team_name = $team_string;
-    $this->save();
-}
-
-
-
-
+    // ↓↓ データベース保存、削除系
+    public function saveTeam($team_string)
+    {
+        $this->team_name = $team_string;
+        $this->save();
+    }
 }
