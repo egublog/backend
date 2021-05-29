4ce7c4359e9322c1e97c4741ca2cb43bc1d7e1a5
23tai <zorn23sky@gmail.com>
2021-05-29 10:48:44 +0900

    Talk_list.phpのリファクタリング（コードの要らない部分を消した）

--- app/Talk.php
+++ app/Talk.php
@@ -6,16 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Talk extends Model
 {
-    //
+    // リレーション ↓↓
     public function user() {
         return $this->belongsTo('App\User', 'from');
     }
 
 
-
-
-    // ↓ scope系
-
+    // ↓↓ scope系
     public function scopeYetColumnsFalse($query, $myId, $user_id)
     {
         return $query->where('from', $user_id)->where('to', $myId)->where('yet', false);
@@ -29,7 +26,6 @@ class Talk extends Model
     public function scopeTalkDataOneBefore($query, $myId, $user_id)
     {
         return $query->where('from', $myId)->where('to', $user_id)->orWhere('from', $user_id)->where('to', $myId)->orderBy('created_at', 'desc')->offset(1)->limit(1);
-        // ->skip(1)でいけるし、、後でfirst()で取得しているから->limit(1)は要らない
     }
     
     public function scopeTalkDataNow($query, $myId, $user_id)
@@ -38,10 +34,7 @@ class Talk extends Model
     }
 
 
-
-
-
-            // ↓ データベース保存、削除系
+    // ↓↓ データベース保存、削除系
     public function saveNewTalk($message, $myId, $user_id)
     {
         $this->talk_data = $message;
