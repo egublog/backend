bd0fa085ef6934bcc097a85e0614ee524ec4c39d
23tai <zorn23sky@gmail.com>
2021-07-21 17:18:02 +0900

    friend.blade.phpのallをeraに変更

--- resources/views/myService/friend.blade.php
+++ resources/views/myService/friend.blade.php
@@ -55,9 +55,9 @@
 
                         </div>
                         <div class="results-body-second">
-                            @foreach($account->alls()->orderBy('id', 'desc')->get() as $all)
+                            @foreach($account->eras()->orderBy('id', 'desc')->get() as $era)
                             <span class="hidden-sp results-body-second-team">
-                                {{ $all->team->team_name }}
+                                {{ $era->team->team_name }}
                                 <span class="hidden-sp">/</span>
                             </span>
                             @endforeach
