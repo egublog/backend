e544d85da4b3c6c9eefc264a01f6a49089362ea9
23tai <zorn23sky@gmail.com>
2021-07-21 17:09:01 +0900

    erasマイグレーションの中身を記述

--- database/migrations/2021_07_21_170426_create_eras_table.php
+++ database/migrations/2021_07_21_170426_create_eras_table.php
@@ -13,19 +13,28 @@ class CreateErasTable extends Migration
      */
     public function up()
     {
+        if (Schema::hasTable('eras')) {
+            // テーブルが存在していればリターン
+            return;
+        }
+        // Schema::create('eras', function (Blueprint $table) {
+        //     $table->bigIncrements('id');
+        //     $table->timestamps();
+        // });
         Schema::create('eras', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('position_id');
+            $table->unsignedBigInteger('team_id')->nullable();
+            $table->unsignedBigInteger('era_id');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            // $table->foreign('position_id')->references('id')->on('positions')->onDelete('cascade');
+            $table->foreign('team_id')->references('id')->on('teams')->onDelete('cascade');
+            // $table->foreign('era_id')->references('id')->on('eras')->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('eras');
-    }
+  
 }
