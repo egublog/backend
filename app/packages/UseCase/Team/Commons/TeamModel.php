c129fd909bab1f856e8de8505019708ca48d04d3
23tai <zorn23sky@gmail.com>
2021-07-22 18:56:42 +0900

    TeamModel.phpを記述

---
+++ app/packages/UseCase/Team/Commons/TeamModel.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\packages\UseCase\User\Commons;
+
+
+// use DateTime;
+// use App\Team as TeamModel;
+// use packages\Domain\Common\Team\Team;
+
+class TeamModel
+{
+    /**
+     * @var int
+     */
+    private $id;
+    /**
+     * @var string
+     */
+    private $team_name;
+
+    /**
+     * User constructor.
+     * @param int $id
+     * @param string $team_name
+     */
+    public function __construct(int $id, string $team_name)
+    {
+        $this->id = $id;
+        $this->team_name = $team_name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTeam_name(): string
+    {
+        return $this->team_name;
+    }
+
+}
