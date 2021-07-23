2d84f3fbad7dad6dfdd9cadaea4f9fc3e21050d3
23tai <zorn23sky@gmail.com>
2021-07-23 17:52:10 +0900

    GetUserAccontEqualToParamRequest.phpを作成と記述

---
+++ app/packages/UseCase/User/Get/GetUserAccontEqualToParamRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\packages\UseCase\User\Get;
+
+
+// use packages\User\Commons\UserModel;
+// use App\packages\UseCase\User\Commons\UserModel;
+
+class GetUserAccontEqualToParamRequest
+{
+    private $user_id;
+
+    /**
+     * UserGetListResponse constructor.
+     * @param int $user_id
+     */
+    public function __construct(int $user_id)
+    {
+        $this->user_id = $user_id;
+    }
+
+    /**
+     * @return int
+     */
+     public function getUser_id(): int
+     {
+         return $this->user_id;
+     }
+
+
+}
