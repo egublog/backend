6fbe9b17e2f15244cfa1319c9dc8164c37c97bf5
23tai <zorn23sky@gmail.com>
2021-07-23 18:10:58 +0900

    details.blade.phpをクリーンアーキテクチャに合わせて修正

--- resources/views/myService/details.blade.php
+++ resources/views/myService/details.blade.php
@@ -13,63 +13,63 @@
 
             <div class="profile-top">
                 <form class="" action="{{ route('backs.from_details') }}" method="GET">
-                    @if ($identify_id == 'talk_find' || $identify_id == 'find')
-                    <input name="team_string" type="hidden" value="{{ $team_string }}">
-                    <input name="era_id" type="hidden" value="{{ $era_id }}">
+                    @if ($viewModel->identify_id == 'talk_find' || $viewModel->identify_id == 'find')
+                    <input name="team_string" type="hidden" value="{{ $viewModel->team_string }}">
+                    <input name="era_id" type="hidden" value="{{ $viewModel->era_id }}">
                     @endif
-                    <input name="user_id" type="hidden" value="{{ $hisAccount->id }}">
-                    <input name="identify_id" type="hidden" value="{{ $identify_id }}">
+                    <input name="user_id" type="hidden" value="{{ $viewModel->hisAccount->id }}">
+                    <input name="identify_id" type="hidden" value="{{ $viewModel->identify_id }}">
                     {{ csrf_field() }}
                     <input class="profile-top-button" type="submit" value="&lt; back">
                 </form>
 
-                <p class="profile-top-tit">{{ $hisAccount->name }}</p>
+                <p class="profile-top-tit">{{ $viewModel->hisAccount->name }}</p>
             </div>
 
             <div class="profile-img">
-                @if ($hisAccount->image === null)
+                @if ($viewModel->hisAccount->image === null)
                 <img src="https://banana2.s3-ap-northeast-1.amazonaws.com/test/E7F5CC7C-E1B0-4630-99B8-DDD050E8E99E_1_105_c.jpeg" alt="">
                 @else
-                <img src="{{ $hisAccount->image }}">
+                <img src="{{ $viewModel->hisAccount->image }}">
                 @endif
             </div>
 
-            @if ($identify_id == 'find')
-            <follow-details-button :identify-id="{{ json_encode($identify_id) }}" :initial-follow-check="{{ json_encode($follow_check) }}" :user-id="{{ json_encode($hisAccount->id) }}" :era-id="{{ json_encode($era_id) }}" :team-string="{{ json_encode($team_string) }}"></follow-details-button>
+            @if ($viewModel->identify_id == 'find')
+            <follow-details-button :identify-id="{{ json_encode($viewModel->identify_id) }}" :initial-follow-check="{{ json_encode($viewModel->hisAccount->follow_check) }}" :user-id="{{ json_encode($viewModel->hisAccount->id) }}" :era-id="{{ json_encode($viewModel->era_id) }}" :team-string="{{ json_encode($viewModel->team_string) }}"></follow-details-button>
             @else
-            <follow-details-button :identify-id="{{ json_encode($identify_id) }}" :initial-follow-check="{{ json_encode($follow_check) }}" :user-id="{{ json_encode($hisAccount->id) }}"></follow-details-button>
+            <follow-details-button :identify-id="{{ json_encode($viewModel->identify_id) }}" :initial-follow-check="{{ json_encode($viewModel->hisAccount->follow_check) }}" :user-id="{{ json_encode($viewModel->hisAccount->id) }}"></follow-details-button>
             @endif
 
             <div class="profile-wrap">
                 <div class="profile-name">
-                    @if($hisAccount->user_name)
-                    <p class="profile-name-txt">{{ $hisAccount->user_name }}</p>
+                    @if($viewModel->hisAccount->user_name)
+                    <p class="profile-name-txt">{{ $viewModel->hisAccount->user_name }}</p>
                     @endif
                 </div>
                 <div class="profile-box">
                     <dl class="profile-def">
-                        @if(isset($hisAccount->age))
+                        @if(isset($viewModel->hisAccount->age))
                         <div class="profile-def-box">
                             <dt class="profile-dtit">age : </dt>
-                            <dd class="profile-data">{{ $hisAccount->age }}</dd>
+                            <dd class="profile-data">{{ $viewModel->hisAccount->age }}</dd>
                         </div>
                         @endif
-                        @foreach($hisAccount->eras as $era)
+                        @foreach($viewModel->hisAccount->eras as $era)
                         
                         <div class="profile-def-box">
-                            <dt class="profile-dtit">{{ $era->changeEraIdToEraName($era->era_id) }} : </dt>
-                            <dd class="profile-data">{{ $era->team->team_name }}</dd>
+                            <dt class="profile-dtit">{{ $era->era_name }} : </dt>
+                            <dd class="profile-data">{{ $era->team_name }}</dd>
                         </div>
                         @endforeach
-                        @if($hisAccount->area_id != '未設定です')
+                        @if($viewModel->hisAccount->area_id != '未設定です')
                         <div class="profile-def-box">
                             <dt class="profile-dtit profile-dtit-area">住んでいるところ : </dt>
-                            <dd class="profile-data"> {{ $hisAccount->changeAreaIdToPrefecturesName($hisAccount->area_id) }}</dd>
+                            <dd class="profile-data"> {{ $viewModel->hisAccount->area_name }}</dd>
                         </div>
                         @endif
                     </dl>
-                    @if(isset($hisAccount->introduction))
-                    <p class="profile-intro">{{ $hisAccount->introduction }}</p>
+                    @if(isset($viewModel->hisAccount->introduction))
+                    <p class="profile-intro">{{ $viewModel->hisAccount->introduction }}</p>
                     @endif
                 </div>
             </div>
